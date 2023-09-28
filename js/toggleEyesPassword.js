e4b3a0d1edfc743e13a0cf2158ca2bebc53e2b2d
KAYITARES <cyntkayitare@gmail.com>
2023-09-28 12:42:33 +0200

    add

---
+++ js/toggleEyesPassword.js
@@ -0,0 +1,12 @@
+const password = document.querySelector("#password");
+const togglePassword = document.querySelector("#togglePassword");
+
+togglePassword.addEventListener("click", function (e) {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+
+  // toggle the icon
+  this.classList.toggle("fas fa-eye");
+});
